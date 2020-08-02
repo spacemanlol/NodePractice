be4c636914803198df713f0b8fe70a71c0afeea2
spacemanlol <shafinschowdhury@gmail.com>
2020-08-02 16:38:47 -0500

    Express app.use

--- nodeExpress/app.js
+++ nodeExpress/app.js
@@ -1,8 +1,5 @@
-// Global module for http
-const http = require('http');
-
 const express = require('express')
-
+const bodyParser = require('body-parser')
 // Import as a function
 const app = express();
 
@@ -10,19 +7,30 @@ const app = express();
 // Add a new middleware function
 // Accepts an array of request handlers
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    // Allows request to travel to next middleware
-    next();
+// app.use((req, res, next) => {
+//     console.log('in the middleware');
+//     // Allows request to travel to next middleware
+//     next();
+// })
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    //console.log('in another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
-    console.log('in another middleware');
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    //console.log('in another middleware');
     res.send('<h1>Hello</h1>')
 })
 
-// App is a valid request handler
-const server = http.createServer(app);
-
 // Creates the server on the given port
-server.listen(3000);
+
+// shortened form with express, creates server and listens
+app.listen(3000);
