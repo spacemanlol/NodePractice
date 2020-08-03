873c67f8e0206415ca3380cce64498d5eb030f40
spacemanlol <shafinschowdhury@gmail.com>
2020-08-03 17:32:21 -0500

    Cart Model

--- nodeAdvancedRoutes/models/cart.js
+++ nodeAdvancedRoutes/models/cart.js
@@ -1,6 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
 module.exports = class Cart {
-    constructor() {
-        this.products = [];
-        
+    static addProduct(id, productPrice){
+        // Fetch previous
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0};
+            if(!err){
+                cart = JSON.parse(fileContent);
+            }
+            // Analyze, find existing
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            if(existingProduct) {
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            }else {
+                updatedProduct = {
+                    id: id,
+                    qty: 1
+                }
+                cart.products = [...cart.products, updatedProduct];
+            }
+            // add new or increase quantity
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            })
+        })
+
+
     }
 }
