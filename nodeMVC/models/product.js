f8d26574afbf50790581bb6dc8ebd98c9741ef27
spacemanlol <shafinschowdhury@gmail.com>
2020-08-03 15:02:21 -0500

    model view controller example complete

--- nodeMVC/models/product.js
+++ nodeMVC/models/product.js
@@ -1,4 +1,20 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    })
+}
+
+
 
 module.exports = class Product {
     constructor(title) {
@@ -7,7 +23,18 @@ module.exports = class Product {
 
     save() {
         // Push current object into array
-        products.push(this);
+        // products.push(this);
+
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
+    }
+
+    static fetchAll(cb){
+        getProductsFromFile(cb)
     }
 
 }
