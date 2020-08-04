37477f4d2c5ff7dd4063bee6513910fd88de8e0c
spacemanlol <shafinschowdhury@gmail.com>
2020-08-03 19:11:41 -0500

    cart

--- nodeAdvancedRoutes/models/cart.js
+++ nodeAdvancedRoutes/models/cart.js
@@ -40,4 +40,39 @@ module.exports = class Cart {
 
 
     }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            // if no cart
+            if(err){
+                return;
+            }
+            const updatedCart = {...JSON.stringify(fileContent) };
+            const product = updatedCart.products.find(prod => prod.id === this.id);
+            const productQty = product.qty;
+
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+                console.log(err);
+            })
+        })
+
+
+
+    }
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err){
+                cb(null);
+            } else {
+                cb(cart);
+            }
+
+        })
+    }
+
 }
